ec3941659ee42c564fd9cc094a2bb86e2678a250
adonnart <alexandre.donnart@gmail.com>
2016-09-16 09:33:52 +0200

    Update authentif_chauffeur.php

--- authentif_chauffeur.php
+++ authentif_chauffeur.php
@@ -1,23 +1,17 @@
 <?php
-$psd=$_POST['psd'];
-$mdp=$_POST['mdp'];
-@$db=mysql_connect('localhost', 'root', '') or die("erreur de connexion".mysql_error());
-mysql_select_db("mlr1", $db) or die("erreur de connexion à la base mlr1");
-@$req="SELECT CHFMDP, CHFPSD FROM chauffeur WHERE CHFPSD = '$psd' AND CHFMDP = '$mdp'";
-$sql=mysql_query($req);
-if(@mysql_numrows($sql)==0)
-{
-	echo"</br>Erreur de connexion";
-	echo'<META HTTP-EQUIV="REFRESH" CONTENT="1; URL=authentif_chauffeur.php">';
-}
-else
-{
-	echo"</br>Connexion réussie";
-	echo'<META HTTP-EQUIV="REFRESH" CONTENT="10; URL=affichage_tournees.php">';
-}
-
-
-
-mysql_close();
-
+	$psd=$_POST['psd'];
+	$mdp=$_POST['mdp'];
+	
+	include 'connectAD.php';
+	
+	@$req="SELECT CHFMDP, CHFPSD FROM chauffeur WHERE CHFPSD = '$psd' AND CHFMDP = '$mdp'";
+	$sql=mysql_query($req);
+	
+	if(@mysql_numrows($sql)==0){
+		echo"</br>Erreur de connexion";
+		echo'<META HTTP-EQUIV="REFRESH" CONTENT="1; URL=authentif_chauffeur.php">';
+	}else{
+		echo"</br>Connexion rÃ©ussie";
+		echo'<META HTTP-EQUIV="REFRESH" CONTENT="10; URL=affichage_tournees.php">';
+	}
 ?>
