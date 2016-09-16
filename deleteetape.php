bf82b17bbb17034da284d33e671c46482f8506da
adonnart <alexandre.donnart@gmail.com>
2016-09-16 11:10:23 +0200

    Update deleteetape.php

--- deleteetape.php
+++ deleteetape.php
@@ -1,20 +1,17 @@
 <?php
-
-if($_POST['submit']) {
-
-	@$db=mysql_connect('localhost', 'root', '') or die("erreur de connexion".mysql_error());
-	mysql_select_db("mlr1", $db) or die("erreur de connexion ‡ la base mlr1");
-
-	$ETPID = $_POST['ETPID'];
-
-	$sql = "DELETE FROM  WHERE ETPID=".$ETPID;
-	$result=mysql_query($sql);
-
-	echo"Etape supprimÈe";
-	echo"<a href='fiche_tournee.php' />";
-	echo'<META HTTP-EQUIV="REFRESH" CONTENT="1; URL=fiche_tournee.php">';
-} else {
-	echo"Erreur";
-}
-
+	if($_POST['submit']) {
+	
+		include "connectAD.php";
+	
+		$ETPID = $_POST['ETPID'];
+	
+		$sql = "DELETE FROM  WHERE ETPID=".$ETPID;
+		$result=mysql_query($sql);
+	
+		echo"Etape supprim√©e";
+		echo"<a href='fiche_tournee.php' />";
+		echo'<META HTTP-EQUIV="REFRESH" CONTENT="1; URL=fiche_tournee.php">';
+	} else {
+		echo"Erreur";
+	}
 ?>
