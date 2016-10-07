4bf521722b08745c4b249a39d5ac891f77ea7e81
15Thomasc <15Thomasc@D22-P08-W7.estransup.local>
2016-10-07 10:56:48 +0200

    test

--- connectAD.php
+++ connectAD.php
@@ -1,32 +1,46 @@
 <?php
 	include "AccesDonnees.php";
-
 	$ip=explode(".",$_SERVER['SERVER_ADDR']);
-
 	switch ($ip[0]) {
 		case 127 :
+		case 192 :
+		//local
 			$host = "127.0.0.1";
 			$user = "root";
 			$password = "";
-			$dbname = "mlr1";
-			$port="";
+			$dbname = "slam4";
+			$port='3306';
 			break;
+			
 		case 31 :
+			//hostinger
 			$host = "mysql.hostinger.fr";
-			$user = "u893031168_root";
-			$password = "estran";
-			$dbname = "u893031168_mlr1";
+			$user = "u627393142_user";
+			$password = "P@ssword";
+			$dbname = "u627393142_slam4";
 			$port='3306';
 			break;
 			
 		default :
-			exit ("Serveur non reconnu.");
+			exit ("Serveur non reconnu...");
 			break;
 	}
+	//connexion ORACLE
+	$host='10.0.220.100';    	// le chemin vers le serveur
+	$port='1521';				// 1521 port pour oracle 3306 pour mysql
+	$dbname='ORAPROF';  	    // le nom de votre base de données ou du service oracle
+	$user='scott';  			// nom d'utilisateur pour se connecter
+	$password='tiger';          // mot de passe de l'utilisateur
+	
 	
 	$connexion=connexion($host,$port,$dbname,$user,$password);
 	
-	if (!$connexion) {
-		echo "Aucune connexion.<br />";
+	if ($connexion) {
+		echo "Connexion reussie<br />";
+		echo "Host : $host:$port<br />";
+		echo "Base $dbname selectionnee... <br />";
+		echo "Utilisateur : $user<br />";
+		echo "Mode acces : $modeacces<br />";
+		echo "Type base : $typebase<hr />";
 	}
 ?>
